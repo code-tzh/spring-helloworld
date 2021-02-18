f90450af9b26d3c728e262889709e0c25055c1ce
code-tzh <75636417+code-tzh@users.noreply.github.com>
2021-02-19 02:08:59 +0300

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,7 +7,7 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        //System.out.println(bean.getMessage());
+        System.out.println(bean.getMessage());
         HelloWorld bean2 =
                 (HelloWorld) applicationContext.getBean("helloworld");
         Cat bean3 =
@@ -15,7 +15,7 @@ public class App {
         Cat bean4 =
                 (Cat) applicationContext.getBean("cat");
 
-        System.out.println(bean.equals(bean2));
-        System.out.println(bean3.equals(bean4));
+        System.out.println(bean == bean2);
+        System.out.println(bean3 == bean4);
     }
 }
